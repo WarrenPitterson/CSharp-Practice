eed88eb15c85a10b31b3b9a1ecfed2bf38f46e88
warren pitterson <warrenpitterson@gmail.com>
2019-04-17 17:12:43 +0100

    c commit

---
+++ Hello/GreetingLogic/Greeting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingLogic
+{
+    public class Greeting
+    {
+        public string GetString()
+        {
+            string greeting = "Why, Hello There!";
+            return greeting;
+        }
+    }
+}
---
+++ Hello/WpfHelo/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using GreetingLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHelo 
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Greeting gl = new Greeting();
+            string message = gl.GetString();
+            MessageBox.Show(message);
+        }
+    }
+}
