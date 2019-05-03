27ca3c79e3cce6363ce160392f8aca2711d16356
warren pitterson <warrenpitterson@gmail.com>
2019-05-03 11:03:27 +0100

    Added Acme App with tests

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages product carried in inventory
+    /// </summary>
+    public class Product
+    {
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private int productID;
+
+        public int ProductID
+        {
+            get { return productID; }
+            set { productID = value; }
+        }
+
+        public string SayHello()
+        {
+            return $"Hello {ProductName} ({ProductID}): {Description}";
+        }
+    }
+}
